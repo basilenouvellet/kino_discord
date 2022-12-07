1c8f12337f630b7e50c9e493c79f3ac5c03c08d4
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-12-07 22:03:56 +0100

    Release v0.1.0

--- mix.exs
+++ mix.exs
@@ -1,29 +1,50 @@
 defmodule KinoSlack.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Slack integration with Livebook"
+
   def project do
     [
       app: :kino_slack,
-      version: "0.1.0",
+      version: @version,
+      description: @description,
+      name: "KinoSlack",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger],
-      mod: {KinoSlack.Application, []}
-    ]
+    [mod: {KinoSlack.Application, []}]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:kino, "~> 0.7"},
-      {:req, "~> 0.3"}
+      {:req, "~> 0.3"},
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "components",
+      source_url: "https://github.com/livebook-dev/kino_slack",
+      source_ref: "v#{@version}",
+      extras: ["guides/components.livemd"]
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/livebook-dev/kino_slack"
+      }
     ]
   end
 end
